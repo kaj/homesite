461d3edf43b92d5dab9ae04036a7ec55583d649d
Rasmus Kaj <kaj@kth.se>
2018-08-16 22:32:05 +0200

    Update style and use warp.

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,77 @@
-extern crate gotham;
-#[macro_use]
-extern crate gotham_derive;
-extern crate hyper;
-#[macro_use]
+extern crate warp;
 extern crate log;
 extern crate mime;
-extern crate serde;
-#[macro_use]
-extern crate serde_derive;
 extern crate env_logger;
+extern crate chrono;
 
-mod ructe_response;
+mod render_ructe;
 
-use gotham::http::response::create_response;
-use gotham::router::builder::{
-    build_simple_router, DefineSingleRoute, DrawRoutes,
-};
-use gotham::router::Router;
-use gotham::state::{FromState, State};
-use hyper::{Response, StatusCode};
-use ructe_response::RucteResponse;
-use templates::*;
+use render_ructe::RenderRucte;
+use chrono::{Utc, Duration};
+use templates::statics::StaticFile;
+use warp::http::{Response, StatusCode};
+use warp::{path, reject, Filter, Rejection, Reply};
 
 fn main() {
     env_logger::init();
-    let addr = "127.0.0.1:3000";
-    gotham::start(addr, router())
-}
 
-pub fn router() -> Router {
-    build_simple_router(|route| {
-        route.get("/").to(homepage);
-        route.get("/gifta").to(married);
-        route.get("/robots.txt").to(robots);
-        route
-            .get("/s/:name")
-            .with_path_extractor::<FilePath>()
-            .to(static_file);
-    })
-}
-
-fn homepage(state: State) -> (State, Response) {
-    state.html(index)
-}
-
-fn married(state: State) -> (State, Response) {
-    state.html(gifta)
-}
-
-fn robots(state: State) -> (State, Response) {
-    let res = create_response(
-        &state,
-        StatusCode::Ok,
-        Some((b"".to_vec(), mime::TEXT_PLAIN)),
+    let router = warp::get2().and(
+        warp::index().and_then(homepage)
+            .or(path("gifta").and_then(married))
+            .or(path("robots.txt").map(robots))
+            .or(path("s").and(path::param()).and_then(static_file))
     );
-    (state, res)
+    warp::serve(router.recover(customize_error)).run(([127, 0, 0, 1], 3030));
 }
 
-#[derive(Deserialize, StateData, StaticResponseExtender)]
-pub struct FilePath {
-    pub name: String,
+fn homepage() -> Result<impl Reply, Rejection> {
+    Response::builder().html(templates::index)
+}
+fn married() -> Result<impl Reply, Rejection> {
+    Response::builder().html(templates::gifta)
+}
+fn robots() -> impl Reply {
+    ""
 }
 
-fn static_file(state: State) -> (State, Response) {
-    let res = {
-        let FilePath { ref name } = FilePath::borrow_from(&state);
-        if let Some(data) = statics::StaticFile::get(&name) {
-            create_response(
-                &state,
-                StatusCode::Ok,
-                Some((data.content.to_vec(), data.mime.clone())),
-            )
-        } else {
-            info!("Static file {} not found", name);
-            create_response(&state, StatusCode::NotFound, None)
+/// Handler for static files.
+/// Create a response from the file data with a correct content type
+/// and a far expires header (or a 404 if the file does not exist).
+fn static_file(name: String) -> Result<impl Reply, Rejection> {
+    if let Some(data) = StaticFile::get(&name) {
+        let far_expires = Utc::now() + Duration::days(180);
+        Ok(Response::builder()
+            .status(StatusCode::OK)
+            .header("content-type", data.mime.as_ref())
+            .header("expires", far_expires.to_rfc2822())
+            .body(data.content))
+    } else {
+        println!("Static file {} not found", name);
+        Err(reject::not_found())
+    }
+}
+
+/// Create custom error pages.
+fn customize_error(err: Rejection) -> Result<impl Reply, Rejection> {
+    match err.status() {
+        StatusCode::NOT_FOUND => {
+            eprintln!("Got a 404: {:?}", err);
+            // We have a custom 404 page!
+            Response::builder().status(StatusCode::NOT_FOUND).html(|o| {
+                templates::error(
+                    o,
+                    StatusCode::NOT_FOUND,
+                    "The resource you requested could not be located.",
+                )
+            })
         }
-    };
-    (state, res)
+        code => {
+            eprintln!("Got a {}: {:?}", code.as_u16(), err);
+            Response::builder()
+                .status(code)
+                .html(|o| templates::error(o, code, "Something went wrong."))
+        }
+    }
 }
 
 include!(concat!(env!("OUT_DIR"), "/templates.rs"));
